bfee721a433eb8e58dc1dd11b526f3957e6a3c9f
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-22 18:01:43 +0900

    Add files via upload

--- js/kakaoOpenApi.js
+++ js/kakaoOpenApi.js
@@ -1,30 +1,202 @@
+//---------- 추천마법사의 선택
 
 $(function () {
-    var books = [
+    var bookTitles = [
         "또 못 버린 물건들",
-        "디케의 눈물",
-        "명탐정의 제물",
-        "소녀 동지여 적을 쏴라",
-        "소설 보다 : 가을 2023",
-        "경험은 어떻게 유전자에 새겨지는가",
-        "흔한남매 이상한 나라의 고전 읽기 4",
-        "사소한 추억의 힘",
-        "문과 남자의 과학 공부",
-        "마시지 않을 수 없는 밤이니까요",
+        "디케",
+        "프로메테우스",
+        "해변의 카프카",
+        "소설 보다",
+        "유전",
+        "고양이",
+        "노르웨이 숲",
+        "잃어버린 시간을",
+        "이토록",
     ];
 
-    books.forEach(function(title){
+    for (let i=0; i<11; i++) {
         $.ajax({
             method: "GET",
             url: "https://dapi.kakao.com/v3/search/book?target=title",
-            data: $.param({ query : title, size: 1 }),
-            headers: { Authorization: "KakaoAK 57053f6a3b08ec813a2b7088ee0a1cf8" }
-        })
-        .done(function (msg) {
-            if (msg.documents.length > 0) {
-                
-            }
-        })
-    })
-})
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section4 .swiperslide .cover").eq(i).append('<a href="#"><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section4 .swiperslide").eq(i).append('<div class="text"><div class="tit"><a href="#">'+ data.documents[i].title +'</a></div></div>')
+        });
+    };
+});
 
+
+//---------- 화제의 책 소식
+
+$(function () {
+    var bookTitles2 = [
+        "달의 아이",
+        "달러 투자",
+        "풍미",
+        "녹색의",
+        "시장",
+        "케인스",
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles2[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section5 .swiperslide a").eq(i).append('<div class="cover"><img src="'+ data.documents[i].thumbnail +'" alt=""></div>');
+            $(".section5 .swiperslide a").eq(i).append('<div class="rtext"><div class="tit">'+ data.documents[i].title +'</div><div class="sub">'+data.documents[i].contents.substring(0,40)+'...'+'</div></div>')
+        });
+    };
+});
+
+
+//---------- 이달의 주목도서
+
+$(function () {
+    var bookTitles = [
+        "챗GPT",
+        "고통",
+        "백년",
+        "일론",
+        "데일카네기",
+        "도시",
+        "결심",
+        "거인",
+        "상황",
+        "향수",
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section7 .swiperslide .cover").eq(i).append('<a href="#"><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section7 .swiperslide").eq(i).append('<div class="text"><div class="tit"><a href="#">'+ data.documents[i].title +'</a></div></div>')
+        });
+    };
+});
+
+
+
+//---------- 이 주의 특가
+
+$(function () {
+    var bookTitles = [
+        "스펀지",
+        "고대 생물",
+        "버리다",
+        "두뇌게임",
+        "메타버스 투자",
+        "요리",
+        "셰익스피어",
+        "글쓰기",
+        "손글씨",
+        "13세",
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section8 .swiperslide .cover").eq(i).append('<a href="#"><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section8 .swiperslide .tit").eq(i).append('<a href="#">'+ data.documents[i].title +'</a>')
+        });
+    };
+});
+
+
+//---------- 알라딘 스페셜
+
+$(function () {
+    var bookTitles = [
+        "4대 비극",
+        "폴 오스터",
+        "슬픈 세상",
+        "말하기",
+        "식탁",
+        "노래",
+        "조지 오웰",
+        "너의 췌장을",
+        "올리브",
+        "내 마음",
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section9 .swiperslide .cover").eq(i).append('<a href="#"><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section9 .swiperslide").eq(i).append('<div class="text"><div class="tit"><a href="#">'+ data.documents[i].title +'</a></div></div>')
+        });
+    };
+});
+
+
+
+//---------- 알라디너의 선택
+
+$(function () {
+    var bookTitles = [
+        "하지말라고는",
+        "철학",
+        "영장",
+        "녹색평론",
+        "고통",
+        "개소리",
+        "영장류",
+        "소크라테스",
+        "생각",
+        "여름",
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section12 .swiperslide .cover").eq(i).append('<a href="#"><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section12 .swiperslide .tit").eq(i).append('<a href="#">'+ data.documents[i].title +'</a>')
+        });
+    };
+});
+
+
+
+//---------- 알라딘 오디오북
+
+$(function () {
+    var bookTitles = [
+        "마시지 않을 수",
+        "고통에 관하여",
+        "알아차림",
+        "빌런",
+        "유목민"
+    ];
+
+    for (let i=0; i<11; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: bookTitles[i] },
+            headers: { Authorization: "KakaoAK 77d5402229f6fe717a05f4568b38aa86"},
+        }).done(function (data) {
+            $(".section13 .swiperslide .cover").eq(i).append('<a href="#"><span class="audiobook">오디오북</span><img src="'+ data.documents[i].thumbnail +'" alt=""></a>');
+            $(".section13 .swiperslide").eq(i).append('<div class="text"><div class="tit"><a href="#">'+ data.documents[i].title +'</a></div></div>')
+        });
+    };
+});
---
+++ js/mainbookshelf.js
@@ -0,0 +1,195 @@
+//---------- 추천마법사의 선택
+
+$(function () {
+    $('.section4 .cover').mouseenter(function(){
+        $(this).find('.bookbtnlayer').show();
+    })
+    $('.section4 .cover').mouseleave(function(){
+        $(this).find('.bookbtnlayer').hide();
+    })
+})
+
+$(function () {
+    $('.section4 .whiteprev').click(function(){
+        $('.section4 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section4 .swiperwrapper');
+        $('.section4 .swiperwrapper').css('margin-left',-1050);
+        $('.section4 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section4 .whitenext').click(function(){
+        $('.section4 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1050}, function(){
+            $('.section4 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section4 .swiperwrapper');
+            $('.section4 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 화제의 책 소식
+
+$(function () {
+    $('.section5 .whiteprev2').click(function(){
+        $('.section5 .swiperwrapper').children('.swiperslide').slice(3).prependTo('.section5 .swiperwrapper');
+        $('.section5 .swiperwrapper').css('margin-left',-1030);
+        $('.section5 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section5 .whitenext2').click(function(){
+        $('.section5 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1030}, function(){
+            $('.section5 .swiperwrapper').children('.swiperslide').slice(0,3).appendTo('.section5 .swiperwrapper');
+            $('.section5 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+//---------- 알라딘이 만든 사은품
+
+$(function () {
+    $('.section6 .whiteprev2').click(function(){
+        $('.section6 .swiperwrapper').children('.swiperslide').slice(3).prependTo('.section6 .swiperwrapper');
+        $('.section6 .swiperwrapper').css('margin-left',-1030);
+        $('.section6 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section6 .whitenext2').click(function(){
+        $('.section6 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1030}, function(){
+            $('.section6 .swiperwrapper').children('.swiperslide').slice(0,3).appendTo('.section6 .swiperwrapper');
+            $('.section6 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 이달의 주목도서
+
+$(function () {
+    $('.section7 .whiteprev').click(function(){
+        $('.section7 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section7 .swiperwrapper');
+        $('.section7 .swiperwrapper').css('margin-left',-1050);
+        $('.section7 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section7 .whitenext').click(function(){
+        $('.section7 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1050}, function(){
+            $('.section7 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section7 .swiperwrapper');
+            $('.section7 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 이 주의 특가
+
+$(function () {
+    $('.section8 .whiteprev').click(function(){
+        $('.section8 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section8 .swiperwrapper');
+        $('.section8 .swiperwrapper').css('margin-left',-1050);
+        $('.section8 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section8 .whitenext').click(function(){
+        $('.section8 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1050}, function(){
+            $('.section8 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section8 .swiperwrapper');
+            $('.section8 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+
+//---------- 알라딘 스페셜
+
+$(function () {
+    $('.section9 .whiteprev').click(function(){
+        $('.section9 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section9 .swiperwrapper');
+        $('.section9 .swiperwrapper').css('margin-left',-1050);
+        $('.section9 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section9 .whitenext').click(function(){
+        $('.section9 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1050}, function(){
+            $('.section9 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section9 .swiperwrapper');
+            $('.section9 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 중간 광고 배너
+
+$(function () {
+    $('.section10 .whiteprev2').click(function(){
+        $('.section10 .swiperwrapper').children('.swiperslide').slice(3).prependTo('.section10 .swiperwrapper');
+        $('.section10 .swiperwrapper').css('margin-left',-1030);
+        $('.section10 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section10 .whitenext2').click(function(){
+        $('.section10 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1030}, function(){
+            $('.section10 .swiperwrapper').children('.swiperslide').slice(0,3).appendTo('.section10 .swiperwrapper');
+            $('.section10 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 투비컨티뉴드 추천 노트
+
+$(function () {
+    $('.section11 .whiteprev2').click(function(){
+        $('.section11 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section11 .swiperwrapper');
+        $('.section11 .swiperwrapper').css('margin-left',-1030);
+        $('.section11 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section11 .whitenext2').click(function(){
+        $('.section11 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1030}, function(){
+            $('.section11 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section11 .swiperwrapper');
+            $('.section11 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
+
+
+
+//---------- 알라디너의 선택
+
+$(function () {
+    $('.section12 .whiteprev').click(function(){
+        $('.section12 .swiperwrapper').children('.swiperslide').slice(5).prependTo('.section12 .swiperwrapper');
+        $('.section12 .swiperwrapper').css('margin-left',-1030);
+        $('.section12 .swiperwrapper').stop().animate({marginLeft:0},400);
+    })
+})
+
+$(function () {
+    $('.section12 .whitenext').click(function(){
+        $('.section12 .swiperwrapper').filter(':not(:animated)').stop().animate({marginLeft:-1030}, function(){
+            $('.section12 .swiperwrapper').children('.swiperslide').slice(0,5).appendTo('.section12 .swiperwrapper');
+            $('.section12 .swiperwrapper').css({marginLeft:0})
+        });
+    })
+})
